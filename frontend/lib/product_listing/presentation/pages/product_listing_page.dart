8a0653439f8a6f06333d2d6a121ad74118af643a
Juliette Barbay <barbayjuliette95@gmail.com>
2023-02-20 21:56:38 +0800

    Create skeleton product listing page

---
+++ frontend/lib/product_listing/presentation/pages/product_listing_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProductListingPage extends StatelessWidget {
+  const ProductListingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Organics e-store'),
+      ),
+      body: const Placeholder(),
+    );
+  }
+}
