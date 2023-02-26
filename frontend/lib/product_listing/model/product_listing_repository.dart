6d344ca1493d4206b810405745037f0fa41f1bd8
Juliette Barbay <barbayjuliette95@gmail.com>
2023-02-26 23:45:17 +0800

    Add filter to listing page
    Filter is based on a search term.

--- frontend/lib/product_listing/model/product_listing_repository.dart
+++ frontend/lib/product_listing/model/product_listing_repository.dart
@@ -4,8 +4,8 @@ import 'package:http/http.dart' as http;
 import './product.dart';
 
 class ProductListingRepository {
-  Future<List<Product>> fetchProducts() async {
-    final url = Uri.http('localhost:3000', '');
+  Future<List<Product>> fetchProducts({required String searchTerm}) async {
+    final url = Uri.http('localhost:3000', 'products', {'search': searchTerm});
     final response = await http.get(url);
     if (response.statusCode == 200) {
       final list = jsonDecode(response.body) as List;
